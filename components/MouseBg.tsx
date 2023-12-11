a2e17da05a560225a53ba9639cf14a5cb765bcbf
BrunoUA96 <ostapklysa@gmail.com>
2023-12-11 20:11:30 +0000

    create mouse position hook

---
+++ components/MouseBg.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useMousePosition } from "@/lib/mouse-position";
+import React from "react";
+
+export const MouseBg = () => {
+  const { x, y } = useMousePosition();
+
+  return (
+    <span
+      style={{ left: x, top: y }}
+      className="absolute -z-20 h-80 w-80 -translate-y-1/2 -translate-x-1/2 bg-sky-200 rounded-full blur-3xl dark:from-sky-900 dark:via-[#0141ff] dark:opacity-40"
+    />
+  );
+};
---
+++ lib/mouse-position.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useMousePosition = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleWindowMouseMove = (event: MouseEvent) => {
+      setCoords({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleWindowMouseMove);
+    };
+  }, []);
+
+  return coords;
+};
