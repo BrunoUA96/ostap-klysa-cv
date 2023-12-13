9566953b52da380feecab128fc0165450ebb9566
BrunoUA96 <ostapklysa@gmail.com>
2023-12-13 19:00:53 +0000

    get project data & list projects

--- components/portfolio/PortfolioCard.tsx
+++ components/portfolio/PortfolioCard.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import React from "react";
+import { Project } from "./PortfolioList";
 
-export const PortfolioCard = () => {
+export const PortfolioCard = ({ ...project }: Project) => {
   return (
     <li className="mb-12">
       <div className="group lg:hover:!opacity-100 lg:group-hover/list:opacity-50 relative">
         <div className="flex gap-4 rounded-md transition-all motion-reduce:transition-none lg:group-hover:bg-slate-300/50 lg:group-hover:-m-4 lg:group-hover:p-4 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg cursor-pointer">
           <div className=" flex-shrink-0">
             <Image
-              src={
-                "https://images.unsplash.com/photo-1702234577513-151004f4cb44?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              }
+              src={project.images[0].url}
               alt="Company"
               width={200}
               height={100}
@@ -18,13 +17,8 @@ export const PortfolioCard = () => {
             />
           </div>
           <div>
-            <h4 className="text-sm font-semibold mb-3">Labrador</h4>
-            <p className="mb-3 text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              quam obcaecati sequi tenetur quibusdam nemo soluta doloribus
-              blanditiis quas earum, corrupti odit ipsa laborum sit eligendi,
-              consequatur molestiae nisi explicabo.
-            </p>
+            <h4 className="text-sm font-semibold mb-3">{project.title}</h4>
+            <p className="mb-3 text-sm">{project.content.text}</p>
           </div>
         </div>
       </div>
--- components/portfolio/PortfolioList.tsx
+++ components/portfolio/PortfolioList.tsx
@@ -1,10 +1,63 @@
 import React from "react";
 import { PortfolioCard } from "./PortfolioCard";
+import { GraphQLClient } from "graphql-request";
 
-export const PortfolioList = () => {
+import { gql } from "graphql-request";
+
+const QUERY = gql`
+  {
+    projects {
+      id
+      title
+      subtitle
+      images {
+        url
+      }
+      content {
+        text
+      }
+    }
+  }
+`;
+
+interface Data {
+  projects: Project[];
+}
+
+export interface Project {
+  id: string;
+  title: string;
+  subtitle?: any;
+  content: {
+    text: string;
+  };
+  images: Image[];
+}
+
+interface Image {
+  url: string;
+}
+
+async function getProjects() {
+  const hygraph = new GraphQLClient(
+    "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clpy9x1jckoj501t0boi1b44e/master"
+  );
+
+  const data = await hygraph.request<Data>(QUERY);
+
+  return { data };
+}
+
+export const PortfolioList = async () => {
+  const { data } = await getProjects();
+
   return (
     <ol className="group/list">
-      <PortfolioCard />
+      {data.projects.map((project) => (
+        <div key={project.id}>
+          <PortfolioCard {...project} />
+        </div>
+      ))}
     </ol>
   );
 };
