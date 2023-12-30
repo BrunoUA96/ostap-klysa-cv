6eb1ece90dbaccf0352aa282904e1c9d6c742d9d
BrunoUA96 <ostapklysa@gmail.com>
2023-12-30 23:36:09 +0000

    style experience card

--- app/components/Experience/ExperienceCard.tsx
+++ app/components/Experience/ExperienceCard.tsx
@@ -5,6 +5,7 @@ import { Card } from "../Card";
 
 import { Experience } from "@/lib/api/dto";
 import { ResponsabilityList } from "../ResponsabilityList";
+import { motion } from "framer-motion";
 
 export const ExperienceCard = (props: Experience) => {
   const [startedYear, startedMonth] = props.startedDate.split("-");
@@ -14,22 +15,24 @@ export const ExperienceCard = (props: Experience) => {
     <Card id={props.id}>
       {/* Timeline Section */}
       <div>
-        <div className="flex gap-3 col-span-2 text-slate-800 text-xs">
-          <span>{`${startedYear}/${startedMonth}`}</span>
-          <span>&mdash;</span>
-          <span>{`${finishYear}/${finishMonth}`}</span>
-        </div>
         <div className="col-span-3">
           {/* Company */}
-          <h5 className="text-sm 2xl:text-md font-bold tracking-wide mb-1">
-            {props.company}
-          </h5>
+          <div className="flex items-center gap-4">
+            <h5 className="text-sm 2xl:text-md font-bold tracking-wide">
+              {props.company}
+            </h5>
+            <div className="flex gap-3 col-span-2 text-slate-800 text-xs lg:group-hover:bg-white/50 px-3 py-1 rounded-lg transition-all">
+              <span>{`${startedYear}/${startedMonth}`}</span>
+              <span>&mdash;</span>
+              <span>{`${finishYear}/${finishMonth}`}</span>
+            </div>
+          </div>
           {/* Position */}
-          <span className="inline-block text-sm font-semibold mb-3">
+          <span className="inline-block text-xs text-sky-800 font-medium mb-3">
             {props.position}
           </span>
           {/* Description */}
-          <p className="mb-3 text-sm">{props.description}</p>
+          <p className="mb-3 text-sm font-light">{props.description}</p>
 
           <ResponsabilityList responsabilityList={props.responsability} />
         </div>
