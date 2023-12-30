7a0dde4a60445d1c4e76de83cb92253f13268d38
BrunoUA96 <ostapklysa@gmail.com>
2023-12-30 22:02:31 +0000

    create taglist components

---
+++ app/components/ResponsabilityList.tsx
@@ -0,0 +1,17 @@
+export const ResponsabilityList = ({
+  responsabilityList,
+}: {
+  responsabilityList?: string[];
+}) => {
+  return (
+    <div className="flex gap-3 flex-wrap">
+      {responsabilityList?.map((res, index) => (
+        <span
+          key={index}
+          className="bg-sky-300/75 px-3 py-1 rounded-full text-xs flex items-center justify-center">
+          {res.replace("_", " ")}
+        </span>
+      ))}
+    </div>
+  );
+};
