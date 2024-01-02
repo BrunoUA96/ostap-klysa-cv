507da68d6565bfe56cbf6512528b60168df98cf6
BrunoUA96 <ostapklysa@gmail.com>
2024-01-02 19:22:51 +0000

    dark mode

--- app/components/MouseBg.tsx
+++ app/components/MouseBg.tsx
@@ -8,7 +8,7 @@ export const MouseBg = () => {
   return (
     <span
       style={{ left: x, top: y }}
-      className="fixed -z-20 h-80 w-80 -translate-y-1/2 -translate-x-1/2 bg-sky-200 rounded-full blur-3xl dark:from-sky-900 dark:via-[#0141ff] dark:opacity-40"
+      className="fixed -z-20 h-80 w-80 -translate-y-1/2 -translate-x-1/2 bg-sky-200 rounded-full blur-3xl dark:bg-sky-800/40"
     />
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={fira_code.className}>
-        <div className="m-auto min-h-screen max-w-screen-xl lg:grid grid-cols-3 gap-4 px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 leading-relaxed">
+      <body
+        className={`${fira_code.className} bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-slate-200`}>
+        <div className="m-auto min-h-screen max-w-screen-xl lg:grid grid-cols-3 gap-4 px-6 md:px-12 md:py-20 lg:px-24 lg:py-0 leading-relaxed">
           <Header />
-          <main className="col-span-2 pt-16 px-16">{children}</main>
+          <main className="col-span-2 pt-5 md:p-8 lg:p-16">{children}</main>
         </div>
       </body>
     </html>
