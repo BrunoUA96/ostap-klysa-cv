1edd67e27908cc1111ff6ddf2f75842075b999a1
BrunoUA96 <ostapklysa@gmail.com>
2024-01-03 16:07:47 +0000

    remove blink onclick

--- app/components/theme/ThemeSwicher.tsx
+++ app/components/theme/ThemeSwicher.tsx
@@ -10,6 +10,7 @@ import { MdDarkMode } from "react-icons/md";
 export const ThemeSwicher = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [blink, setBlink] = useState(true);
 
   const themeHandler = (theme: string) => {
     setTheme(theme);
@@ -24,7 +25,16 @@ export const ThemeSwicher = () => {
   return (
     <Listbox value={theme} onChange={themeHandler}>
       <div className="absolute top-5 right-5 mt-1 z-20">
-        <Listbox.Button className="relative animate-pulse flex items-center gap-2 w-full cursor-pointer rounded-lg bg-sky-300/75 dark:bg-slate-700 dark:text-white text-slate-900 p-2 shadow-md focus:outline-none sm:text-sm">
+        <Listbox.Button
+          onClick={() => setBlink(false)}
+          className="relative flex items-center gap-2 w-full cursor-pointer rounded-lg bg-sky-300/75 dark:bg-slate-700 dark:text-white text-slate-900 p-2 shadow-md focus:outline-none sm:text-sm">
+          {blink && (
+            <span className="absolute -top-2 -right-2 flex h-4 w-4">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-4 w-4 bg-sky-500"></span>
+            </span>
+          )}
+
           <span className="text-xl">
             {theme === "dark" ? (
               <MdDarkMode />
@@ -70,7 +80,7 @@ const OptionList = ({
         `relative cursor-pointer select-none p-2 ${
           active
             ? "bg-slate-400/30 dark:bg-sky-500/30 dark:text-sky-500 text-slate-900"
-            : "text-gray-900"
+            : "text-gray-900 dark:text-white"
         }`
       }
       value={value}>
