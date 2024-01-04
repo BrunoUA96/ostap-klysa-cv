97619f5d3ee56a8db92fa1e8212c88ecd9444ac7
BrunoUA96 <ostapklysa@gmail.com>
2024-01-04 12:50:33 +0000

    divider color

--- app/components/DividerContent.tsx
+++ app/components/DividerContent.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export const DividerContent = () => {
   return (
-    <div className="my-6">
-      <hr />
-    </div>
+    <div className="my-6 border-b border-slate-300 dark:border-slate-700" />
   );
 };
